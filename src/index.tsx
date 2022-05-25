13502577146026826efc6c9fc40e1857173ab504
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-05-24 23:21:19 -0300

    scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./services/firebase";
-import "./styles/global.css";
+import "./styles/global.scss";
 
 
 const root = ReactDOM.createRoot(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import illustration from '../assets/images/illustration.svg'
 import googleIcon from "../assets/images/google-icon.svg"
 import logo from "../assets/images/logo.svg"
+import "../styles/auth.scss"
 
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside >
                 <img src={illustration} alt="Ilustração" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
